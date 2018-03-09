f8d8690eff6fb63583636c3227ba3e04ebb0025c
Pranav Aggarwal <c-pranav.aggarwal@round.glass>
2018-03-09 15:46:51 +0530

    upgraded to ng5

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,23 @@
+let config = {
 
-export default {
-    entry: 'dist/index.js',
-    dest: 'dist/bundles/datatable.umd.js',
-    sourceMap: false,
-    format: 'umd',
-    moduleName: 'ng.datatable',
-    globals: {
-      '@angular/core': 'ng.core',
-      'rxjs/Observable': 'Rx',
-      'rxjs/ReplaySubject': 'Rx',
-      'rxjs/add/operator/map': 'Rx.Observable.prototype',
-      'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-      'rxjs/add/observable/fromEvent': 'Rx.Observable',
-      'rxjs/add/observable/of': 'Rx.Observable'
+    input: 'dist/index.js',
+    output: {
+        file: 'dist/bundles/datatable.umd.js',
+        format: 'umd',
+        sourcemap: false,
+        name: 'ng.datatable',
+        globals: {
+            '@angular/core': 'ng.core',
+            'rxjs/Observable': 'Rx',
+            'rxjs/ReplaySubject': 'Rx',
+            'rxjs/add/operator/map': 'Rx.Observable.prototype',
+            'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
+            'rxjs/add/observable/fromEvent': 'Rx.Observable',
+            'rxjs/add/observable/of': 'Rx.Observable'
+        }
     }
-  }
+  };
+
+
+export default config;
+
--- src/index.ngfactory.ts
+++ src/index.ngfactory.ts
@@ -19,4 +19,4 @@ export const DataTableModuleNgFactory:i0.NgModuleFactory<i1.DataTableModule> = i
           i0.ɵmpd(512,i3.ɵba,i3.ɵba,([] as any[])),i0.ɵmpd(512,i3.FormsModule,i3.FormsModule,
               ([] as any[])),i0.ɵmpd(512,i1.DataTableModule,i1.DataTableModule,([] as any[]))]);
     });
-//# sourceMappingURL=data:application/json;base64,eyJmaWxlIjoiQzovVXNlcnMvYWxleC9Eb2N1bWVudHMvR2l0SHViL2FuZ3VsYXItNC1kYXRhLXRhYmxlLWJvb3RzdHJhcC00L3NyYy9pbmRleC5uZ2ZhY3RvcnkudHMiLCJ2ZXJzaW9uIjozLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJuZzovLy9DOi9Vc2Vycy9hbGV4L0RvY3VtZW50cy9HaXRIdWIvYW5ndWxhci00LWRhdGEtdGFibGUtYm9vdHN0cmFwLTQvc3JjL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbIiAiXSwibWFwcGluZ3MiOiJBQUFBOzs7Ozs7Ozs7Ozs7Ozs7Ozs7OzsifQ==
+//# sourceMappingURL=data:application/json;base64,eyJmaWxlIjoiL1VzZXJzL1BSQU5BVi9Eb2N1bWVudHMvd29ya3N0b3JlL2FuZ3VsYXItNC1kYXRhLXRhYmxlLWJvb3RzdHJhcC00L3NyYy9pbmRleC5uZ2ZhY3RvcnkudHMiLCJ2ZXJzaW9uIjozLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJuZzovLy9Vc2Vycy9QUkFOQVYvRG9jdW1lbnRzL3dvcmtzdG9yZS9hbmd1bGFyLTQtZGF0YS10YWJsZS1ib290c3RyYXAtNC9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiICJdLCJtYXBwaW5ncyI6IkFBQUE7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OyJ9
